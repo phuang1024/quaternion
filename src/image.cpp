e84dfbc0fc687e6f854bbc37b9fcd458255af658
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-10-03 11:12:04 -0700

    Image class.

---
+++ src/image.cpp
@@ -0,0 +1,49 @@
+//
+//  Quaternion
+//  3D raytracer.
+//  Copyright Patrick Huang 2021
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "quaternion.hpp"
+
+
+namespace Quaternion {
+
+
+Image::~Image() {
+    delete[] mem;
+}
+
+Image::Image(int width, int height) {
+    this->width = width;
+    this->height = height;
+    mem = new UCH[width*height*3];
+}
+
+int Image::mempos(int x, int y, int channel) {
+    return y*height*3 + x*3 + channel;
+}
+
+UCH Image::get(int x, int y, int channel) {
+    return mem[mempos(x, y, channel)];
+}
+
+void Image::set(int x, int y, int channel, UCH value) {
+    mem[mempos(x, y, channel)] = value;
+}
+
+
+}
---
+++ src/quaternion.hpp
@@ -0,0 +1,76 @@
+//
+//  Quaternion
+//  3D raytracer.
+//  Copyright Patrick Huang 2021
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+
+namespace Quaternion {
+
+
+// Useful stuff
+typedef  unsigned char  UCH;
+
+
+// Image processing
+
+/**
+ * Image with three channels.
+ */
+struct Image {
+    /**
+     * Deallocates memory.
+     */
+    ~Image();
+
+    /**
+     * Initialize with width and height (pixels).
+     */
+    Image(int width, int height);
+
+    /**
+     * Index of byte to satisfy arguments.
+     */
+    int mempos(int x, int y, int channel);
+
+    /**
+     * Get value at (x, y) and channel.
+     * Channel 0 is R, 1 is G, 2 is B.
+     */
+    UCH get(int x, int y, int channel);
+
+    /**
+     * Set value at (x, y) and channel.
+     * Channel 0 is R, 1 is G, 2 is B.
+     */
+    void set(int x, int y, int channel, UCH value);
+
+    /**
+     * Size is width * height * 3
+     * Three bytes form a pixel.
+     * "width" pixels together form one line on the image.
+     * "height" rows form the whole image.
+     */
+    UCH* mem;
+
+    /**
+     * You can change these, but you shouldn't.
+     */
+    int width, height;
+};
+
+
+}  // namespace Quaternion
